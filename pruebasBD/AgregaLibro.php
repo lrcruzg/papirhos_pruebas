9e996bada21de1351dd6a8b9e41f85cbb48fd20e
Luis Ricardo Cruz <lrcruzg@gmail.com>
2019-07-08 19:01:01 -0500

    no se ingresan datos en este archivo
    aún falta esctibirse practicamente todo

--- pruebasBD/AgregaLibro.php
+++ pruebasBD/AgregaLibro.php
@@ -1,44 +1,25 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <title>Papirhos - Agregar Libro</title>
-        <link type="image/x-icon" href="papirhos_im.ico" rel="icon" />
-    </head>
-    <br>
+<head>
+    <meta charset="utf-8">
+    <title>Papirhos - Agregar Libro</title>
+    <link type="image/x-icon" href="papirhos_im.ico" rel="icon" />
+</head>
+<body>
+    <h1>Agrega Libro</h1>
+    <?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "pruebasBD";
 
-  <body>
-    <h1 align="center">Agregar Libro</h1>
-    <!-- Esta forma agarra el texto a meter en la base de datos-->
-    <form action="agrega_libro.php" method="POST">
-      <fieldset>
-        <legend>Información del libro a agregar</legend>
+    // Crea la conexion
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-        <table align="center">
-            <tr>
-                <td align="right">Título: </td>
-                <td><input type="text" name="tit" required></td>
-            </tr>
+    // Revisa la conexion
+    if ($conn->connect_error) {
+        die("Conexion fallida: " . $conn->connect_error);
+    }
 
-            <tr>
-                <td align="right">Serie: </td>
-                <td><input type="text" name="ser" required></td>
-            </tr>
-
-            <tr>
-                <td align="right">Colección: </td>
-                <td><input type="text" name="colecc" required></td>
-            </tr>
-
-            <tr>
-                <td align="right">Núm de Serie: </td>
-                <td><input type="text" name="numser" required></td>
-            </tr>
-
-        </table>
-        <input type="submit" value="Submit" />
-      </fieldset>
-    </form>
-    <script src="" async defer></script>
-    <br>
-  </body>
+    ?>
+</body>
 </html>

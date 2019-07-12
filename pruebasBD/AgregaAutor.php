1147c8c84f959aeae37b52e0bf78593829083920
Luis Ricardo Cruz <lrcruzg@gmail.com>
2019-07-11 19:54:00 -0500

    funciona con acentos

--- pruebasBD/AgregaAutor.php
+++ pruebasBD/AgregaAutor.php
@@ -1,8 +1,8 @@
 <?php
 //Variables que fueron pasadas por index.html
-$nom = utf8_encode($_POST["Nombre"]);
-$ApellidoP = utf8_encode($_POST["ApellidoPaterno"]);
-$ApellidoM = utf8_encode($_POST["ApellidoMaterno"]);
+$nom = $_POST["Nombre"];
+$ApellidoP = $_POST["ApellidoPaterno"];
+$ApellidoM = $_POST["ApellidoMaterno"];
 
 $servername = "localhost";
 $username = "root";
@@ -11,46 +11,46 @@ $dbname = "pruebasBD";
 
 $message = "";
 
-/*
 // Crea la conexion
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// IMPORTANTE, los acentos no funcionan sin esto
+$acentos = $conn->query("SET NAMES 'utf8'");
 
 // Revisa la conexion
 if ($conn->connect_error) {
     die("Conexion fallida: " . $conn->connect_error);
 }
 
-// <<<<<<<<<< REVISAR A PARTIR DE AQUÍ >>>>>>>>>>>
-
 // verifica si hay otro autor en la base con el mismo nombre y apellidos y los cuenta
 $duplicado = "SELECT COUNT(nombre) AS num FROM autores_aux
-WHERE nombre = '$nom' AND  apellido_paterno = '$ApellidoP' AND apellido_materno = '$ApellidoM'";
+WHERE nombre = '$nom' 
+AND  apellido_paterno = '$ApellidoP' 
+AND apellido_materno = '$ApellidoM'";
 
 $query = mysqli_query($conn, $duplicado);
 
 $row = mysqli_fetch_array($query);
 
-if((int) $row['num'] > 0) {
-    $message = "El autor YA está en la base de datos";
+$esta_repetido = ((int)$row['num'] == 0) ? FALSE : TRUE; 
+ 
+if($esta_repetido) {
+    $message = "El autor ".$nom." ".$ApellidoP." ".$ApellidoM." YA está en la base de datos";
 }
 
-else if((int) $row['num'] == 0) {
-    $message = "El autor NO está en la base de datos";
-    
+else {    
     $sql = "INSERT INTO autores_aux (nombre, apellido_paterno, apellido_materno)
     VALUES ('$nom', '$ApellidoP', '$ApellidoM')";
 
     if ($conn->query($sql) === TRUE) {
-        $message = $message."<br>$nom $ApellidoP $ApellidoM fue agregado a la base de manera exitosa";
+        $message = $message."<br>$nom $ApellidoP $ApellidoM fue agregado a la base de manera exitosa.";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
 }
 
 $conn->close();
 
- */
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +63,6 @@ $conn->close();
     <h1>Agregar Autor</h1>
     <?php
     echo "<h2>$message</h2>";
-    echo "<h2>".$nom." ".$ApellidoP." ".$ApellidoM."</h2>";
     ?>
 </body>
 </html>

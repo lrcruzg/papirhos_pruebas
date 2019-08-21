5ed1d880242296e99cfbb6057f5576045ef81130
Luis Ricardo Cruz <lrcruzg@gmail.com>
2019-08-20 23:25:19 -0500

    los datos del autor se ingresan en este archivo
    los datos del autor se ingresan en este archivo, antes se hacía en el main, también cambió la vista y se agregan los mensajes cambio exitoso (o si el autor ya existia te lo muestra)

--- pruebasBD/AgregaAutor.php
+++ pruebasBD/AgregaAutor.php
@@ -1,11 +1,5 @@
 <?php
-$Nombre = $ApellidoP = $ApellidoM = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $Nombre = test_input($_POST["Nombre"]);
-    $ApellidoP = test_input($_POST["ApellidoPaterno"]);
-    $ApellidoM = test_input($_POST["ApellidoMaterno"]);
-}
+$Nombre = $ApellidoP = $ApellidoM = $message = "";
 
 function test_input($data) {
     $data = trim($data);
@@ -17,58 +11,205 @@ function test_input($data) {
 // crea la conexión a la db
 require_once("db_connect.php");
 
-$message = "";
+if($_POST) {
 
-// IMPORTANTE, los acentos no funcionan sin esto
-$acentos = $conn->query("SET NAMES 'utf8'");
-
-// verifica si hay otro autor en la base con el mismo nombre y apellidos y los cuenta
-$duplicado = "SELECT COUNT(nombre) AS num FROM autores_aux
-WHERE nombre = '$Nombre' 
-AND  apellido_paterno = '$ApellidoP' 
-AND apellido_materno = '$ApellidoM'";
-
-$query = mysqli_query($conn, $duplicado);
-
-$row = mysqli_fetch_array($query);
-
-$esta_repetido = ((int)$row['num'] == 0) ? FALSE : TRUE; 
- 
-if($esta_repetido) {
-    $message = "El autor ".$Nombre." ".$ApellidoP." ".$ApellidoM." YA está en la base de datos";
-}
-
-else {    
-    $sql = "INSERT INTO autores_aux (nombre, apellido_paterno, apellido_materno)
-    VALUES ('$Nombre', '$ApellidoP', '$ApellidoM')";
-
-    if ($conn->query($sql) === TRUE) {
-        $message = $message."<br>$Nombre $ApellidoP $ApellidoM fue agregado a la base de manera exitosa.";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $Nombre = test_input($_POST["Nombre"]);
+        $ApellidoP = test_input($_POST["ApellidoPaterno"]);
+        $ApellidoM = test_input($_POST["ApellidoMaterno"]);
     }
-}
 
-$conn->close();
+    // IMPORTANTE, los acentos no funcionan sin esto
+    $acentos = $conn->query("SET NAMES 'utf8'");
+
+    // verifica si hay otro autor en la base con el mismo nombre y apellidos y los cuenta
+    $duplicado = "SELECT COUNT(nombre) AS num, id_autores FROM autores_aux
+                WHERE nombre = '$Nombre' 
+                AND  apellido_paterno = '$ApellidoP' 
+                AND apellido_materno = '$ApellidoM'";
+
+    $query = mysqli_query($conn, $duplicado);
+
+    $row = mysqli_fetch_array($query);
+
+    $esta_repetido = ((int)$row['num'] == 0) ? FALSE : TRUE;
+
+    $nombre_completo = $Nombre." ".$ApellidoP." ".$ApellidoM; 
+     
+    if($esta_repetido) {
+        $message = 'El autor <a href="DatosAutor.php?autor='.$row['id_autores'].'&nombre='.$nombre_completo.'">'.$nombre_completo.'</a> YA está en la base de datos';
+    } else {    
+        $sql = "INSERT INTO autores_aux (nombre, apellido_paterno, apellido_materno)
+                VALUES ('$Nombre', '$ApellidoP', '$ApellidoM')";
+
+        if ($conn->query($sql) === TRUE) {
+            $message = "$nombre_completo fue agregado a la base de manera exitosa.";
+
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    $conn->close();
+
+}
 
 ?>
 
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Papirhos - Modificar base de datos</title>
+    <title>Papirhos - Agregar Autor</title>
     <link type="image/x-icon" href="papirhos_im.ico" rel="icon" />
-</head>
-<body>
-    <h1>Agregar Autor</h1>
-    <nav>
-        <a href="/PruebasBD/index.html">Inicio</a> |
-        <a href="/PruebasBD/MuestraAutores.php">Autores</a> |
-        <a href="/PruebasBD/MuestraLibros.php">Libros</a> |
-    </nav>
+    <link rel="stylesheet" type="text/css" href="css/menu2.css">
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="stylesheet" type="text/css" href="css/media.css">
+    <link rel="stylesheet" type="text/css" href="css/grid.css">
+    <meta name="viewport" content="initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <style type="text/css">
+        table {
+            padding-left: 23%;
+        }
+        td {
+            width: 40%;
+            padding-bottom: 7px;
+        }
+        li a, .dropbtn {
+            display: inline-block;
+            text-align: center;
+            text-decoration: none;
+        }
+        li a:hover, .dropdown:hover .dropbtn {
+            background-color: none;
+        }
 
-    <?php
-    echo "<h2>$message</h2>";
-    ?>
+        li.dropdown {
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #678;
+            min-width: 150px;
+        }
+
+        .dropdown-content a {
+            text-decoration: none;
+            display: block;
+            text-align: left;
+            padding-left: 4px;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        input[type=text] {
+            background-color: #f2f2f2;
+            width: 90%;
+            border: 1px solid transparent;
+            background-color: #f2f2f2;
+            padding: 12px;
+            font-size: 17px;
+            color: #456;
+        }
+
+        input[type=submit], input[type=reset] {
+            background-color: darkorange;
+            color: #fff;
+            cursor: pointer;
+            border: 1px solid transparent;
+            padding: 12px;
+            font-size: 15px;
+        }
+
+        input[type=number] {
+            padding: 4px;
+        }
+
+        input[type=submit]:hover, input[type=reset]:hover {
+            background-color: #ff8000;
+        }
+
+        .alert {
+            padding: 20px;
+            background-color: #4CAF50;
+            color: white;
+            margin-bottom: 15px;
+        }
+
+        .closebtn {
+            margin-left: 15px;
+            color: white;
+            font-weight: bold;
+            float: right;
+            font-size: 22px;
+            line-height: 20px;
+            cursor: pointer;
+            transition: 0.3s;
+        }
+
+        .closebtn:hover {
+            color: black;
+        }
+    </style>
+</head> 
+<body>
+    <div id="contenedor">
+
+        <div id="encabezado">
+            <div id="logoizq" onclick="window.open('http://www.unam.mx');" style="cursor:pointer;">
+            </div>
+            <div id="logomid">
+            </div>
+            <div id="logoder" onclick="window.open('http://www.matem.unam.mx');" style="cursor:pointer;">
+            </div>
+        </div>
+        
+        <div id="menuencabezado"></div>
+        <script>
+            $(function(){
+                $("#menuencabezado").load("menu_encabezado.html");
+            });
+        </script>
+
+        <div id="contenido">
+            <h1 align="center">Agregar Autor</h1>
+            <form action="AgregaAutor.php" method="POST">
+                <table>
+                    <tr>
+                        <td><input type="text" name="Nombre" placeholder="Ingresa el Nombre" required></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="ApellidoPaterno" placeholder="Ingresa el Apellido Paterno" required></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="ApellidoMaterno" placeholder="Ingresa el Apellido Materno"></td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <input type="reset" value="Reset">
+                            <input type="submit" value="Agregar">                            
+                        </td>                        
+                    </tr>
+                </table>
+            </form>
+
+            <?php
+            if($_POST) {
+                //echo "<h2>$message</h2>";
+                // solo la uso para evitar problemas con los tipos de comillas
+                $aux = "this.parentElement.style.display='none';";
+                echo '<div class="alert">
+                          <span class="closebtn" onclick="'.$aux.'">&times;</span> 
+                          '.$message.'
+                    </div>';
+            }
+            ?>
+        </div>
+    </div>
+
 </body>
 </html>
